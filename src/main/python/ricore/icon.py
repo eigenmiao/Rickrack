29f6f7e8c1f1f3cbd9e50ea030c9a624de01990c
eigenmiao <eigenmiao@outlook.com>
2024-02-18 14:14:32 +0800

    init

--- src/main/python/ricore/icon.py
+++ src/main/python/ricore/icon.py
@@ -125,6 +125,10 @@ __SVG_RIGHT__ = """
 <path style="fill:none;stroke:#{forecolor};stroke-width:2.4;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:{higopacity};stroke-miterlimit:4;stroke-dasharray:none" d="m 3.6355811,5.6652622 6.3346733,6.3347398 -6.3346733,6.33474" />
 """
 
+__SVG_FULL_RIGHT__ = """
+<path style="fill:none;stroke:#{forecolor};stroke-width:2.4;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:{higopacity};stroke-miterlimit:4;stroke-dasharray:none" d="m 9.6355811,5.6652622 6.3346733,6.3347398 -6.3346733,6.33474" />
+"""
+
 __SVG_ZOOM_IN__ = """
 <path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748ZM10 10V7H12V10H15V12H12V15H10V12H7V10H10Z" fill="#{forecolor}"></path>
 """
@@ -188,6 +192,7 @@ def get_icon(name, forecolor, backcolor, higopacity, lowopacity, temp_dir, defau
         "down": __SVG_DOWN__,
         "left": __SVG_LEFT__,
         "right": __SVG_RIGHT__,
+        "full_right": __SVG_FULL_RIGHT__,
         "reset": __SVG_RESET__,
         "zoom_in": __SVG_ZOOM_IN__,
         "zoom_out": __SVG_ZOOM_OUT__,
